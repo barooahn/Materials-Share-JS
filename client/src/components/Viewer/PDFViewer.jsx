4d635c325c078e8213305d938e0d108e86a0d1c4
Nicholas Barooah <barooahn@gmail.com>
2020-12-01 19:26:47 +0000

    Display all pages pdf

--- client/src/components/Viewer/PDFViewer.jsx
+++ client/src/components/Viewer/PDFViewer.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import Pagination from "@material-ui/lab/Pagination";
 import { makeStyles } from "@material-ui/core/styles";
@@ -12,17 +12,18 @@ const useStyles = makeStyles((theme) => ({
 
     canvas: {
       maxWidth: "100% !important",
-      width: "auto !important",
+      width: "100% !important",
       height: "auto !important",
       overflow: "hidden",
     },
   },
 }));
 
-export default ({ file }) => {
+export default ({ file, printPDF, setPrintReady }) => {
   const classes = useStyles();
   const [numPages, setNumPages] = React.useState(null);
   const [pageNumber, setPageNumber] = React.useState(1);
+  const [pdfView, setPdfView] = React.useState(<div></div>);
 
   const pdfWidth = window.screen.width * 0.9;
 
@@ -34,20 +35,57 @@ export default ({ file }) => {
     setPageNumber(page);
   };
 
-  return (
-    <div className={classes.doc}>
-      <React.Fragment>
-        <Document
-          file={file}
-          className={classes.page}
-          onLoadSuccess={onDocumentLoadSuccess}
-          renderMode="canvas">
-          <Page size="A4" pageNumber={pageNumber} maxWidth={"100%"}></Page>
-        </Document>
-        {numPages > 1 ? (
-          <Pagination count={numPages} onChange={onChangePDFpage} />
-        ) : null}
-      </React.Fragment>
-    </div>
-  );
+  // React.useEffect(() => {
+  //   //get all Materials from db setMaterials\
+  //   if (printPDF) {
+  //     console.log("pdf viewer printPDF true", printPDF);
+  //     setPdfView(
+  //       <div className={classes.doc}>
+  //         <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
+  //           {Array.from(new Array(numPages), (el, index) => (
+  //             <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+  //           ))}
+  //         </Document>
+  //       </div>
+  //     );
+  //     setPrintReady(true);
+  //   } else {
+  //     console.log("pdf viewer printPDF false", printPDF);
+  //     console.log("pdf viewer numPages", numPages);
+  //     setPdfView(
+  //       <div className={classes.doc}>
+  //         <React.Fragment>
+  //           <Document
+  //             file={file}
+  //             className={classes.page}
+  //             onLoadSuccess={onDocumentLoadSuccess}
+  //             renderMode="canvas">
+  //             <Page size="A4" pageNumber={pageNumber} maxWidth={"100%"}></Page>
+  //           </Document>
+  //           {numPages > 1 ? (
+  //             <Pagination count={numPages} onChange={onChangePDFpage} />
+  //           ) : null}
+  //         </React.Fragment>
+  //       </div>
+  //     );
+  //     setPrintReady(false);
+  //   }
+  // }, [printPDF, numPages, pageNumber]);
+
+
+  React.useEffect(() => {
+    //get all Materials from db setMaterials\
+
+      setPdfView(
+        <div className={classes.doc}>
+          <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
+            {Array.from(new Array(numPages), (el, index) => (
+              <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+            ))}
+          </Document>
+        </div>
+      );
+
+  }, [printPDF, numPages, pageNumber]);
+  return pdfView;
 };
