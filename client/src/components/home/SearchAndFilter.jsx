652777bf495ca4e24c23f28416cda980c62b8179
Nicholas Barooah <barooahn@gmail.com>
2021-08-31 08:52:04 +0100

    fixed images

--- client/src/components/home/SearchAndFilter.jsx
+++ client/src/components/home/SearchAndFilter.jsx
@@ -16,7 +16,7 @@ const useStyles = makeStyles((theme) => ({
 	img: {
 		backgroundSize: "cover",
 		background:
-			"url(./img/DocsPicsVidsImg-80.webp)  no-repeat center center",
+			"url(./img/searchAndFilterImg.webp)  no-repeat center center",
 		backgroundAttachment: "fixed",
 		minHeight: "100vh",
 	},
