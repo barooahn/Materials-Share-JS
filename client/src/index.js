141188ed5a9f76182f320a4b0b50cc821291db3a
Nicholas Ronald Barooah <barooahn@gmail.com>
2022-10-12 16:43:39 +0100

    more service worker and css

--- client/src/index.js
+++ client/src/index.js
@@ -20,7 +20,8 @@ import {
 	Switch,
 	Redirect,
 } from "react-router-dom";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./service-worker";
+import { register } from "./serviceWorkerRegistration";
 import Axios from "axios";
 
 const MaterialStepper = lazy(() =>
@@ -150,4 +151,4 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+register();
