319fd50e9a2a8667f3267f583a66b47090ab96b2
barooahn <barooahn@gmail.com>
2023-03-18 00:32:54 +0000

    fixed icons and about page

--- client/src/components/home/DocsPicsVids.jsx
+++ client/src/components/home/DocsPicsVids.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { makeStyles } from "@mui/styles";
 import Typography from "@mui/material/Typography";
-
-import Grid from "@mui/material/Grid";
+import { List, ListItem } from "@mui/material";
 
 const useStyles = makeStyles((theme) => ({
 	documentsPicturesVideo: {
-		margin: "20px 0",
+		margin: "3rem 0",
 		[theme.breakpoints.up("sm")]: {
 			margin: "0 -24px",
 		},
@@ -23,31 +22,29 @@ const useStyles = makeStyles((theme) => ({
 	mainText: {
 		backgroundColor: "#003f5c",
 		color: "white",
-		padding: "3rem 1rem",
-		margin: "10px auto",
+		margin: "0 auto",
+		padding: "2rem",
+		// textAlign: "center",
 		[theme.breakpoints.up("sm")]: {
-			padding: "2rem",
-			textAlign: "left",
-			maxWidth: "66%",
+			maxWidth: "50%",
 			fontSize: "1.2rem",
 		},
 	},
 	proTipContainer: {
-		padding: "20px",
-		margin: "10px auto",
-		borderRadius: "5px 0 0 0",
-		paddingBottom: "10px",
+		padding: "1.5rem",
+		marginTop: "1rem",
+		paddingBottom: "2rem",
 		[theme.breakpoints.up("sm")]: {
-			width: "50%",
+			maxWidth: "30%",
+			marginRight: "2rem",
+			marginLeft: "auto",
+			textAlign: "right",
 		},
 	},
 	proTip: {
 		fontSize: "1.2rem",
 		fontWeight: 700,
 		textTransform: "capitalize",
-		paddingBottom: "10px",
-
-		[theme.breakpoints.up("sm")]: {},
 	},
 
 	tipText: {
@@ -75,48 +72,45 @@ export default function DocsPicsVids() {
 				className={classes.title}
 				align='center'
 				variant='h1'
-				component='h1'>
+				component='h1'
+			>
 				Documents, Pictures and Video
 			</Typography>
 
-			<br />
-			<Grid
-				container
-				spacing={1}
-				justifyContent='center'
-				alignItems='center'>
-				<Grid item sm={9} xs={12}>
-					<Typography
-						className={classes.mainText}
-						variant='body1'>
-						Upload Word documents, PDF files, vidoes, images
-						to create the perfect teaching resource.
-						<br />
-						<br />
-						Give additional details to help your material be
-						easily found using search and filter.
-					</Typography>
-					<div className={classes.proTipContainer}>
-						<Typography
-							className={classes.proTip}
-							variant='h2'
-							component='h3'
-							color='secondary'>
-							PRO TIP
+			<div className={classes.mainText}>
+				<List>
+					<ListItem>
+						<Typography variant='body1'>
+							Upload Word documents, PDF files, videos,
+							images to create the perfect teaching
+							resource.
 						</Typography>
-						<Typography
-							className={classes.tipText}
-							variant='body2'>
-							Why not complete all the fields when
-							uploading a resource and make your own
-							resource book? Just print and send to the
-							publishers. Useful day-to-day, attractive to
-							employers, and evidence of professional
-							development.
+					</ListItem>
+					<ListItem>
+						<Typography variant='body1'>
+							Give additional details to help your material
+							be easily found using search and filter.
 						</Typography>
-					</div>
-				</Grid>
-			</Grid>
+					</ListItem>
+				</List>
+			</div>
+			<div className={classes.proTipContainer}>
+				<Typography
+					className={classes.proTip}
+					variant='h3'
+					component='h3'
+					color='secondary'
+				>
+					PRO TIP
+				</Typography>
+				<Typography className={classes.tipText} variant='body2'>
+					Why not complete all the fields when uploading a
+					resource and make your own resource book? Just print
+					and send to the publishers. Useful day-to-day,
+					attractive to employers, and evidence of professional
+					development.
+				</Typography>
+			</div>
 		</div>
 	);
 }
--- client/src/components/home/SaveSecurelyForever.jsx
+++ client/src/components/home/SaveSecurelyForever.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { makeStyles } from "@mui/styles";
 import Typography from "@mui/material/Typography";
-import Grid from "@mui/material/Grid";
+import { List, ListItem } from "@mui/material";
 
 const useStyles = makeStyles((theme) => ({
 	saveSecurelyForever: {
-		margin: "20px 0",
+		margin: "3rem 0",
 		[theme.breakpoints.up("sm")]: {
 			margin: "0 -24px",
 		},
@@ -22,31 +22,26 @@ const useStyles = makeStyles((theme) => ({
 	mainText: {
 		backgroundColor: "#7a5195",
 		color: "white",
-		padding: "3rem 1rem",
-		margin: "10px auto",
+		margin: "0 auto",
+		padding: "2rem",
 		[theme.breakpoints.up("sm")]: {
-			padding: "2rem",
-			textAlign: "left",
-			maxWidth: "66%",
+			maxWidth: "50%",
 			fontSize: "1.2rem",
 		},
 	},
 	proTipContainer: {
-		padding: "20px",
-		margin: "10px auto",
-		borderRadius: "5px 0 0 0",
-		paddingBottom: "10px",
+		padding: "1.5rem",
+		marginTop: "1rem",
+		paddingBottom: "2rem",
 		[theme.breakpoints.up("sm")]: {
-			width: "50%",
+			maxWidth: "30%",
+			marginLeft: "2rem",
 		},
 	},
 	proTip: {
 		fontSize: "1.2rem",
 		fontWeight: 700,
 		textTransform: "capitalize",
-		paddingBottom: "10px",
-
-		[theme.breakpoints.up("sm")]: {},
 	},
 
 	tipText: {
@@ -56,9 +51,11 @@ const useStyles = makeStyles((theme) => ({
 	},
 	title: {
 		color: "black",
-		padding: "30px 10px",
+		paddingTop: "1.5rem",
+		paddingBottom: "1.5rem",
 		[theme.breakpoints.up("sm")]: {
-			paddingTop: "25px",
+			paddingTop: "2rem",
+			paddingBottom: "2rem",
 		},
 	},
 }));
@@ -69,7 +66,6 @@ export default function DocsPicsVids() {
 	return (
 		<div className={classes.saveSecurelyForever}>
 			<div className={classes.img}> </div>
-
 			<Typography
 				className={classes.title}
 				align='center'
@@ -79,45 +75,39 @@ export default function DocsPicsVids() {
 				Save Securely, Forever
 			</Typography>
 
-			<br />
-			<Grid
-				container
-				spacing={1}
-				justifyContent='center'
-				alignItems='center'
-			>
-				<Grid item sm={8} xs={12}>
-					<Typography
-						className={classes.mainText}
-						variant='body1'
-					>
-						Always carry your work with you with cloud
-						storage. Anywhere you can access the internet, you
-						can access your resources.
-					</Typography>
-					<div className={classes.proTipContainer}>
-						<Typography
-							className={classes.proTip}
-							variant='h2'
-							component='h3'
-							color='secondary'
-						>
-							PRO TIP
+			<div className={classes.mainText}>
+				<List>
+					<ListItem>
+						<Typography variant='body1'>
+							Always carry your work with you with cloud
+							storage.
 						</Typography>
-						<Typography
-							className={classes.tipText}
-							variant='body2'
-						>
-							Look out for the padlock on websites to make
-							sure your data is secure. Insecure websites
-							can access data you do not want to share.
-							Material Share only shares your teaching
-							resources with your permission and never
-							shares your personal data.
+					</ListItem>
+					<ListItem>
+						<Typography variant='body1'>
+							Anywhere you can access the internet, you can
+							access your resources.
 						</Typography>
-					</div>
-				</Grid>
-			</Grid>
+					</ListItem>
+				</List>
+			</div>
+			<div className={classes.proTipContainer}>
+				<Typography
+					className={classes.proTip}
+					variant='h3'
+					component='h3'
+					color='secondary'
+				>
+					PRO TIP
+				</Typography>
+				<Typography className={classes.tipText} variant='body2'>
+					Look out for the padlock on websites to make sure your
+					data is secure. Insecure websites can access data you
+					do not want to share. Material Share only shares your
+					teaching resources with your permission and never
+					shares your personal data.
+				</Typography>
+			</div>
 		</div>
 	);
 }
